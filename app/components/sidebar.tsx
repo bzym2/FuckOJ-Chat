a806fcf8e6994a3a892061848aa81d71b41b7db8
BIZONUSERYAKAMO <2860483411@qq.com>
2025-03-14 14:04:05 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -250,8 +250,8 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="FuckOJ"
+        subTitle="最权威的AIGC-OJ工具"
         logo={<ChatGptIcon />}
         shouldNarrow={shouldNarrow}
       >
